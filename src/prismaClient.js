3dcf9703c9b6b381b63176b416e86eb02d4c883e
hdesumo <honoredesumo@yahoo.com>
2025-09-15 01:44:16 +0200

    fix: restore prismaClient.js and move to src/

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,8 +1,8 @@
+// src/prismaClient.js
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export { prisma };
